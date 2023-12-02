3ed05ee9692eb738e5a93c9ec85386c30260f53b
shamad <fuzzriff@gmail.com>
2023-12-03 02:52:35 +0300

    simplified

--- 10-allow-func/index.ts
+++ 10-allow-func/index.ts
@@ -2,25 +2,19 @@ function AllowFunc(func: (value: any) => boolean) {
   return (target: any, propertyKey: string | symbol): void => {
     let value: any = target[propertyKey];
 
-    const setter = function (newValue: any) {
-      if (func(newValue)) {
-        value = newValue;
-      } else {
-        throw new Error(
-          `Ошибка присвоения значения для ${String(
-            propertyKey
-          )}. Разрешенные значения определяются функцией ${String(func)}`
-        );
-      }
-    };
-
-    const getter = function () {
-      return value;
-    };
-
     Object.defineProperty(target, propertyKey, {
-      set: setter,
-      get: getter,
+      set: (newValue) => {
+        if (func(newValue)) {
+          value = newValue;
+        } else {
+          throw new Error(
+            `Ошибка присвоения значения для ${String(
+              propertyKey
+            )}. Разрешенные значения определяются функцией ${String(func)}`
+          );
+        }
+      },
+      get: () => value,
     });
   };
 }
@@ -33,8 +27,8 @@ class User {
 const person = new User();
 console.log(person.age);
 
-person.age = 0;
+person.age = 20;
 console.log(person.age);
 
-person.age = 20;
+person.age = 0;
 console.log(person.age);
